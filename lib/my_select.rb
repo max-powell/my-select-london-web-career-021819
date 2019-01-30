d37d66fdeab205073f8522af471778d8c46b0ba2
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-30 12:52:25 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,8 +4,12 @@ def my_select(collection)
     i = 0
     while i < collection.length
       if yield(collection[i])
+        new_collection.push(collection[i])
+      end
+      i += 1
     end
   else
     puts "No block given"
   end
+  new_collection
 end
